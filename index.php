aa098b930ac72736694ade562ec086b9d6b86a35
Witcha Chimphlee <26061828+Vijjagit@users.noreply.github.com>
2025-05-27 10:07:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<?php
+require_once 'includes/header.php';
+?>
 
+<div class="card">
+    <div class="card-header">
+        <h4>ระบบจัดการข้อมูลบุคลากร</h4>
+    </div>
+    <div class="card-body">
+        <a href="employee/create.php" class="btn btn-primary mb-3">เพิ่มบุคลากรใหม่</a>
+        <a href="employee/read.php" class="btn btn-info mb-3">แสดงรายชื่อบุคลากรทั้งหมด</a>
+    </div>
+</div>
+
+<?php
+require_once 'includes/footer.php';
+?>
