0248c97f2996216c84ba22a0c5ff91adb0b1c474
Witcha Chimphlee <26061828+Vijjagit@users.noreply.github.com>
2025-05-27 10:04:04 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+// ไฟล์นี้ไม่ควร commit ขึ้น GitHub (ให้เพิ่มในไฟล์ .gitignore)
+return [
+    'db' => [
+        'host' => 'localhost',
+        'username' => 'root', // เปลี่ยนเป็นชื่อผู้ใช้จริง
+        'password' => '', // เปลี่ยนเป็นรหัสผ่านจริง
+        'database' => 'cyber_staff_db'
+    ]
+];
