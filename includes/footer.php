66c1032b9dfc416ba9719ee541ae407f5d9dfe57
Witcha Chimphlee <26061828+Vijjagit@users.noreply.github.com>
2025-05-27 10:06:05 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,5 @@
-
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
